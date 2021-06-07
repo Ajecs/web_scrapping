21de67e040330af9f1884573b356c30a30d26004
ajecs <nico7coriale@gmail.com>
2021-06-06 23:31:45 -0300

    peticion especie de pokemon - link de rasgos pokemon - obtener tabla -> 3ra columna - impresion de todos los datos

--- main.py
+++ main.py
@@ -12,13 +12,22 @@ if __name__ == '__main__':
         soup = BeautifulSoup(content, 'html.parser')
 
         table = soup.find('table', {'id': 'pokedex'})
-        for row in table.tbody.find_all('tr'):
+        for row in table.tbody.find_all('tr', limit=5):
             columns = row.find_all('td', limit=3)  # limite de nodos a obtener
-            
+
             name = columns[1].a.text
-            type = [a.text for a in columns[2].find_all('a')] # * list comprehension
-            # print(*type) # * transforma el resultado(list) en un string
+            type = [a.text for a in columns[2].find_all('a')]
+            
 
             link = DOMAIN + columns[1].a['href']
 
-            print (link)
+            pokemon_response = requests.get(link)
+            if pokemon_response.status_code == 200:
+                pokemon_content = pokemon_response.text
+
+                pokemon_soup = BeautifulSoup(pokemon_content, 'html.parser')
+                pokemon_table = pokemon_soup.find('table', class_='vitals-table')
+
+                species = pokemon_table.tbody.find_all('tr')[2].td.text
+                
+                print(name, '|', *type, '|', species)
